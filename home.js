a0d7ee68fd5899a76909ee95368925b90f1fc2e0
Glitch (iris-apple-shade) <none>
2024-08-07 18:42:38 +0000

    🌜🪆 Updated with Glitch

--- home.js
+++ home.js
@@ -15,12 +15,15 @@ window.onload = function() {
       createElement.style.height = 180 + size + "px"; 
       createElement.style.left = Math.random() * innerWidth + "px"; 
       createElement.style.top = (window.innerHeight + 100) + "px";
+      createElement.style.zindex = -1;
 
+    /*
       createElement.style.pointerEvents = "auto"; // Ensure pointer events are enabled
 
       createElement.onclick = function() { 
           this.remove(); // Remove the clicked element
       };
+    */
 
       section.appendChild(createElement); 
 
